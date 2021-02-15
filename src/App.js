904342e80d208fe04a0139d022b5d922a4bdf53a
choihyoungsuk <devchoigun@gmail.com>
2021-02-15 10:59:35 +0900

    20210215-01

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
-import "./App.css";
-import R059_FetchGet from "./R059_FetchGet";
-import "bootstrap/dist/css/bootstrap.css";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import StrAddButton from "./StrAddButton";
 
-function App() {
-  return (
-    <div>
-      <h1>Start React 200!</h1>
-      <p>CSS 적용하기</p>
-      <R059_FetchGet />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Start React 200!</h1>
+        {/*<span>{this.props.store.getState().data.str}</span>*/}
+        <span>{this.props.str}</span>
+        <br />
+        {/*<StrAddButton store={this.props.store} /> */}
+        <StrAddButton AppProp="200" />
+      </div>
+    );
+  }
 }
+
+let mapStateToProps = (state, props) => {
+  console.log("Props from index.js : " + props.indexProp);
+  return {
+    str: state.data.str,
+  };
+};
+
+App = connect(mapStateToProps, null)(App);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
 
-ReactDOM.render(
-  <App />,
+const CallMiddleware = (store) => (nextMiddle) => (action) => {
+  console.log("1. reducer 실행 전");
+  console.log(
+    "2. action.type : " +
+      action.type +
+      ", store str : " +
+      store.getState().data.str
+  );
+  let result = nextMiddle(action);
+  console.log("3. reducer 실행 후");
+  console.log(
+    "4. action.type : " +
+      action.type +
+      ", store str : " +
+      store.getState().data.str
+  );
+  return result;
+};
 
-  document.getElementById("root")
-);
+const store = createStore(reducers, applyMiddleware(CallMiddleware));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const listener = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App indexProp="react" />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+
+store.subscribe(listener);
+listener();
